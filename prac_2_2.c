bb571f365777759cdf7a0513b11eeb30c9a24a17
denmark-dangangui <dlrgks88@naver.com>
2022-09-15 12:16:29 +0900

    2_2_주석처리 및 삽입정렬 수정

--- prac_2_2.c
+++ prac_2_2.c
@@ -5,19 +5,51 @@
 #pragma warning(disable:4996)
 
 int *queue;
-int queuesize, front, rear;
+int queuesize, front, rear,cnt;
 
-void enque(){
-
+void enqueue(int num){
+    if(cnt == 0){
+        queue[rear] = num;
+        cnt++;
+    }
+    else{
+        rear = (rear + 1) % queuesize;
+        queue[rear] = num;
+    }
 }
 
 void print(){
-
+    for(int i = 0 ; i < queuesize ; i++){
+        printf("%d ",queue[i]);
+    }
+    printf("\n");
 }
 
-void main(){
-    int n, i;
+int main(){
+    int n, i, j, num, key, idx;
+    cnt = 0;
     scanf("%d",&queuesize);
     queue = (int *)malloc(sizeof(int) * queuesize);
-    
+    for(i = 0 ; i < queuesize ; i++){
+        queue[i] = 0;
+    }
+    for(i = 0 ; i < queuesize; i++){
+        scanf("%d",&num);
+        enqueue(num);
+    }
+    for(i = 1 ; i < queuesize ; i++){
+        key = queue[i];
+        for(j = i - 1; j >= 0; j--){
+            if(queue[j] > key){                 // 이부분부터 정렬 다시 생각 
+                queue[j + 1] = queue[j];
+            }
+            else{ 
+                break;
+            }
+        }
+        queue[j+1] = key;
+        print();
+
+    }
+    free(queue);
 }
